0be3dd0200c8d403b26608103fca80aaa2df3034
hamsemare <30583701+hamsemare@users.noreply.github.com>
2017-08-20 00:16:10 -0600

    Fixed sign up password bug

--- config/env/default.js
+++ config/env/default.js
@@ -59,7 +59,7 @@ module.exports = {
     owasp: {
       allowPassphrases: true,
       maxLength: 128,
-      minLength: 10,
+      minLength: 6,
       minPhraseLength: 20,
       minOptionalTestsToPass: 4
     }
